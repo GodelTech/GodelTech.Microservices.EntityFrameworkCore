be12b8415626842d46c48bc3e24e3e45f26efe5c
Andei Salanoi <a.salanoi@godeltech.com>
2020-04-23 18:04:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GodeTech.Microservices.EntityFrameworkCore
+
+### Microservice Entity Framework adapter
+
+This package contains repository pattern implementation for EF Core framework.
---
+++ test/GodelTech.Microservices.EntityFrameworkCore.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GodelTech.Microservices.EntityFrameworkCore.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
